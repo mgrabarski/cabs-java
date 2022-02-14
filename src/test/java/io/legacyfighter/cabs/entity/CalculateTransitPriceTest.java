574074e955bb7c6004708b5ec94f36acd5a78015
Mateusz <mateusz.grabarski@gmail.com>
2022-02-14 20:22:37 +0100

    Added CalculateDriverFeeServiceTest.java

---
+++ src/test/java/io/legacyfighter/cabs/entity/CalculateTransitPriceTest.java
@@ -0,0 +1,75 @@
+package io.legacyfighter.cabs.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class CalculateTransitPriceTest {
+
+    @Test
+    void canNotCalculateTransitWhenItsNotCompleted() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.IN_TRANSIT);
+
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(transit::calculateFinalCosts);
+    }
+
+    @Test
+    void canNotEstimateCostWhenTransitIsCompleted() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.COMPLETED);
+
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(transit::estimateCost);
+    }
+
+    @Test
+    void calculatesCostForRegularDay() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.COMPLETED);
+        transit.setDateTime(LocalDate.of(2022, 2, 14).atStartOfDay().toInstant(ZoneOffset.UTC));
+
+        Integer result = transit.calculateFinalCosts();
+
+        assertThat(result).isEqualTo(900);
+    }
+
+    @Test
+    void calculatesCostForWeekendDay() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.COMPLETED);
+        transit.setDateTime(LocalDate.of(2022, 2, 13).atStartOfDay().toInstant(ZoneOffset.UTC));
+
+        Integer result = transit.calculateFinalCosts();
+
+        assertThat(result).isEqualTo(1000);
+    }
+
+    @Test
+    void calculateEstimateCostForRegularDay() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.DRAFT);
+        transit.setDateTime(LocalDate.of(2022, 2, 14).atStartOfDay().toInstant(ZoneOffset.UTC));
+
+        Integer result = transit.estimateCost();
+
+        assertThat(result).isEqualTo(900);
+    }
+
+    @Test
+    void calculateEstimateCostForWeekendDay() {
+        Transit transit = new Transit();
+        transit.setStatus(Transit.Status.DRAFT);
+        transit.setDateTime(LocalDate.of(2022, 2, 13).atStartOfDay().toInstant(ZoneOffset.UTC));
+
+        Integer result = transit.estimateCost();
+
+        assertThat(result).isEqualTo(1000);
+    }
+}
